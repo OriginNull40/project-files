4a0c7561be09e5d5c29143afc782c25638d7113a
OriginNull40 <originnull26@gmail.com>
2026-02-05 12:28:13 -0800

    Update webrcade-core.js

--- webrcade-core.js
+++ webrcade-core.js
@@ -6,9 +6,13 @@ window.WebRcadePlayer = class {
     async loadFeed(feedUrl) {
         const root = document.getElementById(this.instanceId);
         
-        // We are pointing to YOUR NEW FOLDER now
+        // We use a relative path. This tells the browser: 
+        // "Stay in the current folder, just go into the 'player' directory."
+        // This avoids using any 'webrcade.github.io' links that Blocksi hates.
         const playerFrame = document.createElement('iframe');
-        playerFrame.src = `player/index.html?feed=${feedUrl}`;
+        
+        // This is the relative path to your new file
+        playerFrame.src = "./player/index.html?feed=" + encodeURIComponent(feedUrl);
         
         playerFrame.style.width = "100%";
         playerFrame.style.height = "100vh";
@@ -16,5 +20,6 @@ window.WebRcadePlayer = class {
         
         root.innerHTML = ""; 
         root.appendChild(playerFrame);
+        console.log("Iframe redirected to local player room.");
     }
 };
